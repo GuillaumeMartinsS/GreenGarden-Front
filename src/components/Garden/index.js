e4c7597eb742c68d2dca351f9edb6ddb13362397
GuillaumeMartinsS <guillaume_martins@sfr.fr>
2024-03-29 22:28:57 +0100

    Garden first draft done

--- src/components/Garden/index.js
+++ src/components/Garden/index.js
@@ -1,9 +1,21 @@
+import {Button} from "semantic-ui-react";
+
+import Plant from '../Cards/Plant';
 import './styles.scss';
 
 const Garden = () => (
 
   <main className='main__container'>
-    
+    <Button color="teal" size="mini" className='main__button'>Mettre à jour</Button>
+    <div className='main__garden'>
+      <Plant></Plant>
+      <Plant></Plant>
+      <Plant></Plant>
+      <Plant></Plant>
+      <Plant></Plant>
+      <Plant></Plant>
+    </div>
+
   </main>  
 
 )
