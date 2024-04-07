db516e21d506cfea70cda654b5990a81e63ef4f9
GuillaumeMartinsS <guillaume_martins@sfr.fr>
2024-04-07 21:23:27 +0200

    Conditional view added for Plant component + some CSS changes

--- src/components/Cards/Plant.js
+++ src/components/Cards/Plant.js
@@ -16,14 +16,9 @@ const Plant = ({
         <div className='article__age'> Âge de la plante : {age}</div>
         <div className='article__hydration'> Hydratation : {hydration}</div>
         <div className='article__genre'> Genre : {genre}</div>
-      </div>
-      {/* Si une plante est présente, proposer d'arroser : */}
+        </div>
       <Button color="blue" size="mini" className='article__hydrate'>
             Arroser</Button>
-      {/* Si pas de plante présente, proposer d'en planter une : */}
-      <Button color="blue" size="mini" className='article__plant'>
-            Planter</Button>
-    
   </article>  
 
 )
--- src/components/Garden/index.js
+++ src/components/Garden/index.js
@@ -28,20 +28,21 @@ const Garden = () => {
   console.log('je lis le state');
   console.log(plants);
 
-
   return (
 
   <main className='main__container'>
     <Button color="blue" size="large" className='main__button' Horizontally Attached="right">Mettre à jour</Button>
     <div className='main__garden'>
-       {plants.map((plant) => (
+       {plants && (plants.map((plant) => (
           <Plant
             key={plant.id}
             age={plant.age}
             hydration={plant.hydration}
             genre={plant.genre.name}  
           />
-      ))}
+      )))}
+
+
     </div>
 
   </main>  )
